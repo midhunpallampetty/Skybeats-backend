287c3592e24dd9bb3d9659968d4ec8dc1b89925f
midhunpallampetty <midhunpallampetty@gmai.com>
2024-11-06 20:56:58 +0530

    after tunnel

--- src/graphql/resolvers/flightResolver.ts
+++ src/graphql/resolvers/flightResolver.ts
@@ -1,26 +1,17 @@
 import { IResolvers } from '@graphql-tools/utils';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import { HttpsProxyAgent } from 'https-proxy-agent';
 
 const fetchFlightData = async (fromAirport: string, toAirport: string) => {
     console.log(fromAirport, toAirport);
     const URL = `https://www.cleartrip.com/flight-schedule/${fromAirport}-${toAirport}-flights.html`;
 
     try {
-        const userAgents = [
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15",
-            "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1",
-        ];
+       
+     
 
-        const randomUserAgent = userAgents[Math.floor(Math.random() * userAgents.length)];
-
-        // Await the response from axios.get
-        const response = await axios.get(URL, {
-            headers: {
-                'User-Agent': randomUserAgent,
-            },
-        });
+        const response = await axios.get(URL)
 
         console.log('type of response', typeof response.data);
         const html: any = response.data;
